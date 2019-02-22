1baef33381145c6e8079537ad9ecd27a605ca934
Nate Wang <supnate@gmail.com>
2019-02-22 15:02:07 +0800

    Use hash id plugin for webpack so that dll is always compatible with plugins.

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -258,7 +258,7 @@ module.exports = {
     // <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
     // In production, it will be an empty string unless you specify "homepage"
     // in `package.json`, in which case it will be the pathname of that URL.
-
+    new webpack.HashedModuleIdsPlugin(),
     new InterpolateHtmlPlugin(env.raw),
     // Generates an `index.html` file with the <script> injected.
     new HtmlWebpackPlugin({
