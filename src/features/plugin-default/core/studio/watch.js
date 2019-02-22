106a2b2feaadd5616d783edd5e057754c84abcca
Nate Wang <supnate@gmail.com>
2019-02-22 11:21:13 +0800

    Emit config change when .rekit or package.json are changed.

--- src/features/plugin-default/core/studio/watch.js
+++ src/features/plugin-default/core/studio/watch.js
@@ -1,9 +1,14 @@
-const { vio, files } = rekit.core;
+const { vio, files, config } = rekit.core;
 
 function watchFileChange(io) {
   files.on('change', changes => {
     vio.reset();
     if (io) io.emit('fileChanged', { timestamp: files.lastChangeTime });
+  });
+
+  config.on('change', changes => {
+    vio.reset();
+    if (io) io.emit('configChanged', {});
   });
 }
 
