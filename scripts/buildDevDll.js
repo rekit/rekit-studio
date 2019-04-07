cbd98d62ffd36e961ce5045be7121f362a2a629e
Nate Wang <supnate@gmail.com>
2019-04-07 16:46:33 +0800

    Build dev dll.

---
+++ scripts/buildDevDll.js
@@ -0,0 +1,43 @@
+process.env.NODE_ENV = 'development';
+
+const fs = require('fs-extra');
+const webpack = require('webpack');
+const paths = require('../config/paths');
+
+function buildDevDll() {
+  const dllName = 'rsdevdll';
+
+  console.log('Building Rekit Studio dev dll...');
+  fs.emptyDirSync(paths.resolveApp('dev-dll'));
+
+  const config = require('../config/webpack.config')('development');
+  config.output.filename = `${dllName}.js`;
+  config.output.library = dllName;
+  config.output.path = paths.resolveApp('dev-dll');
+  config.plugins.push(
+    new webpack.DllPlugin({
+      path: paths.devDllManifest,
+      name: dllName,
+      context: paths.appSrc,
+    }),
+  );
+
+  console.time('Rekit Studio dev dll build success');
+
+  return new Promise((resolve, reject) => {
+    webpack(config, err => {
+      if (err) {
+        console.log('Rekit Studio dev dll build failed:');
+        console.log(err.stack || err);
+        reject();
+        return;
+      }
+      console.timeEnd('Rekit Studio dev dll build success');
+      resolve();
+    });
+  });
+}
+
+buildDevDll();
+
+module.exports = buildDevDll;
