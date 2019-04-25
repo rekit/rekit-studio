710e8b80d68e54e86797899259c0091da5fd1220
Nate Wang <supnate@gmail.com>
2019-04-25 22:30:21 +0800

    Fix plugin build from sdk.

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -233,7 +233,11 @@ module.exports = function(webpackEnv, args = {}) {
       // We placed these paths second because we want `node_modules` to "win"
       // if there are any conflicts. This matches Node resolution mechanism.
       // https://github.com/facebook/create-react-app/issues/253
-      modules: [paths.resolveApp('node_modules'), 'node_modules'].concat(
+      modules: [
+        args.pluginDir
+          ? path.join(args.pluginDir, 'node_modules')
+          : paths.resolveApp('node_modules'),
+      ].concat(
         // It is guaranteed to exist because we tweak it in `env.js`
         process.env.NODE_PATH.split(path.delimiter).filter(Boolean),
       ),
