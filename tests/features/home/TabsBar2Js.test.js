ba6dabb8630d19eb4418f0780a095494754e3a80
Nate Wang <supnate@gmail.com>
2019-04-28 11:08:20 +0800

    Added back beautiful dnd.

--- tests/features/home/TabsBar2Js.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import { TabsBar2Js } from '../../../src/features/home';
-
-it('renders node with correct class name', () => {
-  const renderedComponent = shallow(<TabsBar2Js />);
-  expect(renderedComponent.find('.home-tabs-bar-2-js').length).toBe(1);
-});
