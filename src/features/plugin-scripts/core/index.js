661096fe9b6853137438df5f260bc27977c149b2
Nate Wang <supnate@gmail.com>
2019-04-30 01:22:58 +0800

    Implement shouldUse for plugin to filter itself.

--- src/features/plugin-scripts/core/index.js
+++ src/features/plugin-scripts/core/index.js
@@ -1,9 +1,14 @@
+const fs = require('fs');
 const studio = require('./studio');
 const app = require('./app');
 
 const plugin = {
+  name: 'scripts',
   studio,
   app,
+  shouldUse() {
+    return fs.existsSync(rekit.core.paths.map('package.json'));
+  }
 };
 
 module.exports = plugin;
--- src/features/plugin-test/core/index.js
+++ src/features/plugin-test/core/index.js
@@ -1,4 +1,8 @@
+/* global rekit */
 module.exports = {
   name: 'test',
   studio: require('./studio'),
+  shouldUse() {
+    return !!rekit.core.config.getRekitConfig().rekitTest;
+  }
 };
