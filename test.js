f31bf05e45ba91f9dbb709a03fb57bdb665ea162
Nate Wang <supnate@gmail.com>
2019-05-12 21:14:42 +0800

    Code clean.

--- test.js
+++
@@ -1,7 +0,0 @@
-const git = require('simple-git')(process.cwd());
-// git.cwd('/Users/pwang7/workspace4/abc')
-git.status(console.log);
-
-git.checkIsRepo((...args) => {console.log(args)});
-
-git.diff((err, stat) => console.log(stat));
