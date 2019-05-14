9855263124f199b9013df0b70d945260b01930b4
Nate Wang <supnate@gmail.com>
2019-05-15 00:39:58 +0800

    Refine default deps diagram.

--- src/features/diagram/selectors/getElementsForDepsDiagram.js
+++ src/features/diagram/selectors/getElementsForDepsDiagram.js
@@ -3,8 +3,13 @@ import { createSelector } from 'reselect';
 export default createSelector(
   a => a,
   elementById => {
+    const byId = id => elementById[id];
     return Object.values(elementById)
-      .filter(ele => ele.deps)
+      .filter(
+        ele =>
+          ele.deps ||
+          (ele.parts && ele.parts.some && ele.parts.some(pid => byId(pid) && byId(pid).deps)),
+      )
       .map(ele => ele.id);
   },
 );
