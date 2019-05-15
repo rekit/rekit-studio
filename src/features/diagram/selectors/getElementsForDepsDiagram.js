2daf24e60113ccc2e338142be041e990584bdbc3
Nate Wang <supnate@gmail.com>
2019-05-15 21:48:19 +0800

    Refine deps diagram view.

--- src/features/diagram/selectors/getElementsForDepsDiagram.js
+++ src/features/diagram/selectors/getElementsForDepsDiagram.js
@@ -1,15 +1,23 @@
+import _ from 'lodash';
 import { createSelector } from 'reselect';
 
 export default createSelector(
   a => a,
   elementById => {
     const byId = id => elementById[id];
-    return Object.values(elementById)
-      .filter(
-        ele =>
-          ele.deps ||
-          (ele.parts && ele.parts.some && ele.parts.some(pid => byId(pid) && byId(pid).deps)),
-      )
+    const ids = Object.values(elementById)
+      .filter(ele => ele.deps)
       .map(ele => ele.id);
+
+    // For grouped element, use it instead of its parts.
+    Object.values(elementById)
+      .filter(
+        ele => ele.parts && ele.parts.some && ele.parts.some(pid => byId(pid) && byId(pid).deps),
+      )
+      .forEach(ele => {
+        _.pullAll(ids, ele.parts);
+        ids.push(ele.id);
+      });
+    return ids;
   },
 );
